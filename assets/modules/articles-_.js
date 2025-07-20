56d27e1d527ff24a4e883fe3a581f1836fe56822
Deb Axom <debaxom@gmail.com>
2025-07-20 17:15:06 +0530

    Removed Windows Support

--- assets/modules/articles-_.js
+++ assets/modules/articles-_.js
@@ -1,1 +1,1 @@
-import{component_default as n}from"./component-39lQS.js";var o=new n(async function(){return'<div class="container" id="routes-slot-/articles" n-scope="3923f7"></div>'},async function(){});export{o as default};
+import{component_default as n}from"./component-Djs.js";var o=new n(async function(){return'<div class="container" id="routes-slot-/articles" n-scope="ojs0g"></div>'},async function(){});export{o as default};
